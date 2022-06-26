da2f003e93e52a515dead3ed432251db951922d2
Nicholas igunbor <nickoklaus5@gmail.com>
2022-06-26 14:10:52 +0100

    commit to main

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var my_name = "My name is Nicholas Igunbor."
-var height = "I'm 5 feet and 9 inches tall."
+var my_name = "My name is Nicholas Igunbor"
+var height = "I'm 5 feet and 9 inches tall"
 var country = "I am from Nigeria."
 
-document.write('Name : '+ my_name + '<br>' +'Height : ' + height + '<br>' + 'Country : ' + country);
+document.write(my_name + ', ' + height + ', ' + country);
